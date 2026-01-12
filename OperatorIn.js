ee898d7eedaa194d7f510da15c0e9e2417a04044
AyushRaj-Bytes <ayushrajyadav.tech@gmail.com>
2026-01-12 23:37:28 +0530

    OperatorIn.js

---
+++ OperatorIn.js
@@ -0,0 +1,77 @@
+// Date:12Jan2026
+
+// Operators In js
+
+//Arithmatic Operrators (+ , - , * , ** , / , %)
+
+// let num1 = 10;
+// let num2 = 5;
+// console.log(num1, " + ", num2, " = " ,num1+num2);           // Addition
+// console.log(num1, " - ", num2, " = " ,num1-num2);           // Subtraction
+// console.log(num1, " * ", num2, " = " ,num1*num2);           // Multiplication
+// console.log(num1, " ** ", num2, " = " ,num1**num2);         // nthPower
+// console.log(num1, " / ", num2, " = " ,num1/num2);           // Division
+// console.log(num1, " % ", num2, " = " ,num1%num2);           // Modulus
+// console.log("++",num1, " = " ,++num1);
+// console.log("--",num1, " = " ,--num1);
+
+
+
+
+// Assignment Operator
+
+// let num = 20;
+// console.log("num=20, +=num :", num+=num)
+// console.log("num=20, -=num :", num-=num)
+// console.log("num=20, *=num :", num*=num)
+// console.log("num=20, **=num :", num**=num)
+
+
+
+
+
+//Comparison Operator( ==, ===, !=, !==, <, >, <=, >=)
+
+// num1 = 369;
+// num2 = 369;
+// console.log(num1==num2);
+
+// num3 = 36;
+// num4 = "36"; 
+// console.log(num3===num4);
+
+// num5 = 12;
+// num6 = 13;
+// console.log(num5 != num6);
+
+// num7 = 46;
+// num8 = 58;
+// console.log(num7 !== num8);
+
+// num9 = 30;
+// num10 = 29;
+// console.log(num9<num10);
+
+// num11 = 78;
+// num12 = 83;
+// console.log(num11>num12);
+
+// num13 = 93;
+// num14 = 93;
+// console.log(num13<=num14);
+
+// num15 = 29;
+// num16 = 39;
+// console.log(num15>=num16);
+
+
+
+// Logical Operator (&&, ||, !, Symbol)
+
+num1 = 83;
+num2 = 53;
+num3 = 37;
+console.log(num1 > num2 && num2 > num3)  //true
+console.log(num1 < num2 || num2 < num3)  //false
+console.log(!(num1 < num2))
+
